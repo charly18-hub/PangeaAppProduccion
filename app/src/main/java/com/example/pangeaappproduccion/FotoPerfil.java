520cddad0c2f8f7b91506d1213afe8bfa3818ee2
charly <karloos895@gmail.com>
2022-04-08 00:47:24 -0500

    perfil,busquedas y listas commit

---
+++ app/src/main/java/com/example/pangeaappproduccion/FotoPerfil.java
@@ -0,0 +1,2 @@
+package com.example.pangeaappproduccion;public class FotoPerfil {
+}
---
+++ app/src/main/java/com/example/pangeaappproduccion/PerfilBuscado.java
@@ -0,0 +1,14 @@
+package com.example.pangeaappproduccion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilBuscado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_buscado);
+    }
+}
